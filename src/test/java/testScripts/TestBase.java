ad28b0d44c9e157c07abecbdeb39bbadbeb7b5a9
krishnakTechnocretids <kananikrishna427@gmail.com>
2020-09-01 20:43:01 +0530

    remove parameter from Test base

--- src/test/java/testScripts/TestBase.java
+++ src/test/java/testScripts/TestBase.java
@@ -10,11 +10,6 @@ import pages.ResultPage;
 
 public class TestBase extends TestNGListeners {
 
-	public static String defaultBrowser = "Chrome";
-	public static String defaultenvironment = "QA";
-	public static String browserName = System.getProperty("browser");
-	public static String environmentName = System.getProperty("environment");
-
 	HomePage homePage = HomePage.getHomePage();
 	ResultPage resultPage = ResultPage.getResultPage();
 	ProductDetailPage productDetailPage = ProductDetailPage.getProductDetailPage();
